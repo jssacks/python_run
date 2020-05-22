0de9ceba2e3d4be5e9843459022cd3187d73d823
Jade Sauvé <64500436+jade-sauve@users.noreply.github.com>
2020-05-22 12:24:49 -0700

    File creation

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+
+PMEC final project
+Team: Python Run
+Team Members: Jade Sauvé
+			  Joshua Sacks
+			  Maria Kuruvilla
+			  Irita Aylward
+
+Main body of the project
+
+"""
+
+# Modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#import datetime
+
+import os
+import sys
+pht = os.path.abspath('/../python_run')
+if pht not in sys.path:
+    sys.path.append(pht)
+from modules import *
+
+
+######################## Parameters ########################
+
+directory_in = ''  
+
+
+save = True  # True or False, to save
+directory_out = ''
+file_out = ''  
+
+##############################################################
---
+++ modules.py
@@ -0,0 +1,18 @@
+"""
+
+PMEC final project
+Team: Python Run
+Team Members: Jade Sauvé
+			  Joshua Sacks
+			  Maria Kuruvilla
+			  Irita Aylward
+
+Contains the modules used in main_body.py
+
+"""
+
+# Modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import datetime
