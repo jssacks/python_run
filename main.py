2a7c55e98938b08d0b218fb5deb5d6e07105fe2e
Jade Sauvé <64500436+jade-sauve@users.noreply.github.com>
2020-05-26 16:19:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -36,27 +36,6 @@ file_out = ''
 
 ##############################################################
 
-# # read in geodatabase
-# import fiona
-# import geopandas as gpd
-
-# fiona.listlayers(file_geo)
-
-# # Download from https://data.cityofnewyork.us/City-Government/Projected-Sea-Level-Rise/6an6-9htp directly...
-# data = gpd.read_file(file_geo, driver='FileGDB', layer=1)
-
-
-
-"""
-Notes:
-
-https://api.ptagis.org/Swagger/ui/index#/
-
-an xml file...
-ftp://ftp.ptagis.org/RawDataFiles/Interrogation/Loaded/ACM/2020/ACM-2020-002-P-001.xml
-"""
-
-
 
 
 
